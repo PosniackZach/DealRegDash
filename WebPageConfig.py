9da92ba5131850aa03362d26a2669a6e18b5f47d
PosniackZach <zposniack@gmail.com>
2019-09-18 13:13:30 -0700

    Add to home page html

--- WebPageConfig.py
+++ WebPageConfig.py
@@ -8,9 +8,18 @@ def home():
     <head>
         <meta charset="UTF-8">
         <title>Reggie's Home</title>
+        <style>
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
     </head>
     <body style="background-color:#0F0118 ;">
-        <h1 style="color:white;text-align:left;font-size:200%">DealReg Dash</h1>
+        <h1 style="color:white;text-align:center;font-size:200%">DealReg Dash</h1>
+        <img src="https://newevolutiondesigns.com/images/freebies/robot-wallpaper-2.jpg" alt="Reggie" width="1000" height="300" border="10" style="border-color:#BD08D6;">
+        <h2
     </body>
 </html>'''
 
